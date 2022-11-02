7e3f4ad45f221e65d0e6eaf4b80bdcdb73288051
soojiin <111262028+soojiin@users.noreply.github.com>
2022-11-02 23:20:46 +0900

    2nd

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,64 +1,17 @@
 function solution(dartResult) {
+    let bonus = {'S': 1, 'D': 2, 'T': 3},
+        options = {'*': 2, '#': -1, '': 1};
     
-    let score = [], bonusOpt = [], cnt = 0;
+    let darts = dartResult.match(/\d{1,}[SDT][*#]?/g);
     
-    for (let i = 0 ; i < dartResult.length ; i++) {
-        if (dartResult[i] === '1' && dartResult[i+1] === '0') { score.push(10); i += 1; }
-        else if (dartResult[i] !== 'S' && dartResult[i] !== 'D' && dartResult[i] !== 'T' && dartResult[i] !== '*' && dartResult[i] !== '#') { score.push(Number(dartResult[i])); }
-        else { bonusOpt.push(dartResult[i]); }
+    for (let i = 0 ; i < darts.length ; i++) {
+        let split = darts[i].match(/(\d{1,})([SDT])([*#]?)/),
+            score = Math.pow(split[1], bonus[split[2]]) * options[split[3]];
+        
+        if (split[3] === '*' && darts[i - 1]) darts[i - 1] *= options['*'];
+        
+        darts[i] = score;
     }
     
-    for (let i = 0 ; i < bonusOpt.length ; i++) {
-        if (bonusOpt[i] === 'S') {
-            if (bonusOpt[i+1] === '*') {
-                if (score[cnt-1]) {
-                    score[cnt-1] *= 2;
-                    score[cnt] *= 2;
-                }
-                else {
-                    score[cnt] *= 2;
-                }
-            }
-            else if (bonusOpt[i+1] === '#') {
-                score[cnt] *= -1;
-            }
-            cnt++;
-        }
-        if (bonusOpt[i] === 'D') {
-             score[cnt] = Math.pow(score[cnt], 2);
-            if (bonusOpt[i+1] === '*') {
-                if (score[cnt-1]) {
-                    score[cnt-1] *= 2;
-                    score[cnt] *= 2;
-                }
-                else {
-                    score[cnt] *= 2;
-                }
-            }
-            else if (bonusOpt[i+1] === '#') {
-                score[cnt] *= -1;
-            }
-            cnt++;
-        }
-            
-        if (bonusOpt[i] === 'T') {
-            score[cnt] = Math.pow(score[cnt], 3);
-            if (bonusOpt[i+1] === '*') {
-                if (score[cnt-1]) {
-                    score[cnt-1] *= 2;
-                    score[cnt] *= 2;
-                }
-                else {
-                    score[cnt] *= 2;
-                }
-            }
-            else if (bonusOpt[i+1] === '#') {
-                score[cnt] *= -1;
-            }
-            cnt++;
-        }
-            
-    }
-    
-    return score.reduce((acc, cur) => acc + cur);
+    return darts.reduce((a, b) => a + b);
 }
