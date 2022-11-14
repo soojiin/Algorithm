c6a756f6eeb4b6ac98415a756b3e06eee93d8319
soojiin <111262028+soojiin@users.noreply.github.com>
2022-11-14 10:38:57 +0900

    Update 푸드 파이트 대회.js

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,9 +1,7 @@
 function solution(food) {
     let res = '';
     for (let i = 1 ; i < food.length ; i++) {
-        for (let j = 0 ; j < Math.floor(food[i] / 2) ; j++) {
-            res += i;
-        }
+        res += String(i).repeat(Math.floor(food[i]/2));
     }
     return res + 0 + res.split('').reverse().join('');
 }
