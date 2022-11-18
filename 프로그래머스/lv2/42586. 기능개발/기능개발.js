83bddceefeaa84e93d30e9c180da53b8fc972cad
soojiin <111262028+soojiin@users.noreply.github.com>
2022-11-19 00:05:47 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -0,0 +1,23 @@
+function solution(progresses, speeds) {
+    // queue
+    let days = [];
+    
+    progresses.forEach((el, idx) => {
+        days.push(Math.ceil((100 - el) / speeds[idx]))
+    });
+    
+    let cur = days[0];
+    let cnt = 1, res = [];
+        
+    for (let i = 1 ; i < days.length ; i++) {
+        if (cur >= days[i]) cnt++;
+        else { 
+            res.push(cnt);
+            cur = days[i];
+            cnt = 1;
+        }
+    }
+    res.push(cnt);
+    
+    return res;
+}
