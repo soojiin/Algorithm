4d5668db6399b81d48eb9a1c81b7341ad67c573f
Sojin Kim <111262028+soojiin@users.noreply.github.com>
2022-11-24 12:48:59 +0900

    Update 다리를 지나는 트럭.js

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,6 +1,5 @@
 function solution(bridge_length, weight, truck_weights) {
     
-    // let cur = truck_weights.shift();
     let queue = new Array(bridge_length).fill(0);
     let cnt = 0, queue_weight = 0;
     
@@ -9,27 +8,7 @@ function solution(bridge_length, weight, truck_weights) {
     queue_weight += truck_weights[0];
     queue.push(truck_weights.shift());
     
-    // let queue = [cur], cnt = 1, queue_weight = cur; // [7]
-    
     while (queue_weight) {
-//         if (queue.length === bridge_length) {
-//             let passed = queue.shift(); //4
-//             queue_weight -= passed; //5
-//         }
-        
-//         cur = truck_weights[0]; //6 10
-        
-//         if (queue_weight + cur <= weight) {
-//             queue_weight += cur;
-//             truck_weights.shift(); // []
-//             queue.push(cur); //[10]
-//         }
-//         else {
-//             let passed = queue.shift(); // 5
-//             queue_weight -= passed; //0
-//         }
-        
-//         cnt++;
         cnt++;
         
         queue_weight -= queue.shift();
